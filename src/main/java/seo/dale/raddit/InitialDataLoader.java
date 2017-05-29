f96f7f381a1bd5c0229571730bad5427d2c91410
Dale Seo <dale.seo@gmail.com>
2017-05-29 16:33:01 +0900

    add InitialDataLoader

---
+++ src/main/java/seo/dale/raddit/InitialDataLoader.java
@@ -0,0 +1,39 @@
+package seo.dale.raddit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Random;
+import java.util.Scanner;
+
+@Component
+public class InitialDataLoader implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(InitialDataLoader.class);
+
+    private final TopicRepository repository;
+
+    @Autowired
+    public InitialDataLoader(TopicRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+	    File mockData = new ClassPathResource("mock_topics.dat").getFile();
+        Scanner scanner = new Scanner(new FileInputStream(mockData));
+        Random random = new Random();
+        while (scanner.hasNext()) {
+            Topic topic = new Topic(scanner.nextLine(), random.nextInt(10), random.nextInt(10));
+            logger.info("{}", topic);
+            repository.save(topic);
+        }
+    }
+
+}
