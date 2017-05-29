e0450a3ab1dfa1c3085c8d5e73757eb390e3298f
Dale Seo <dale.seo@gmail.com>
2017-05-29 21:33:58 +0900

    not load initial data on production

--- src/main/java/seo/dale/raddit/InitialDataLoader.java
+++ src/main/java/seo/dale/raddit/InitialDataLoader.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 @Component
+@Profile("!production")
 public class InitialDataLoader implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(InitialDataLoader.class);
